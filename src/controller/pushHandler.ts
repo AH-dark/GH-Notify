b7c494afd8ff28ae7fc425df76c6cfeace4ad8e4
AH-dark <ahdark@outlook.com>
2022-06-17 23:58:17 +0800

    Feat: filter for @dependabot

--- src/controller/pushHandler.ts
+++ src/controller/pushHandler.ts
@@ -3,8 +3,16 @@ import Push from "../model/gh-hook/event/push";
 import dayjs from "dayjs";
 
 const PushHandler = async (data: Push, sender: Sender) => {
+    if (data.sender.login === "dependabot[bot]") {
+        // Dependabot Filter, avoid too much messages.
+        await sender.sendTextMessage(
+            `仓库 ${data.repository.name}${data.repository.private ? " (Private)" : ""} 依赖已自动更新。`
+        );
+        return;
+    }
+
     let msg = [
-        `**有新的 <font color="warning">${data.forced ? "Force " : ""}Push</font> 事件**`,
+        `**有新的 <font color="warning">${data.forced ? "Force " : ""}Push</font> 事件**`, // Basic messages
         `仓库: [${data.repository.name}](${data.repository.html_url})${data.repository.private ? " (Private)" : ""}`,
         `提交者：[${data.sender.login}](${data.sender.html_url})`,
     ];
@@ -12,6 +20,7 @@ const PushHandler = async (data: Push, sender: Sender) => {
     msg.push("");
 
     if (data.commits.length > 0) {
+        // Commit Info messages
         data.commits.map((commit) => {
             msg.push(`> Commit ${commit.id.slice(0, 12)}`);
             commit.message.split("\n").map((message) => {
